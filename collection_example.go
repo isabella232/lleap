9dc6f69282bf8875c6f1265f25bc44759da3172e
Ludovic Barman <lbarman@protonmail.ch>
2018-01-31 13:54:15 +0100

    Collection example refactor (still not working)

--- collection_example.go
+++ collection_example.go
@@ -7,27 +7,27 @@ import (
 
 func main() {
 
-	c := collections.EmptyCollection(collections.Data{})
+	collection := collections.EmptyCollection(collections.Data{})
 
 	/*
 	 * CRUD
 	 */
 
 	// Creates a record
-	c.Add([]byte("record"), []byte("data"))
+	collection.Add([]byte("record"), []byte("data"))
 
 	// Remove throws an error on non-existing keys
-	err := c.Remove([]byte("record-nonexisting"))
+	err := collection.Remove([]byte("record-nonexisting"))
 	if err != nil {
 		fmt.Println("Expected error (key not found):", err)
 	}
 
 	// Creates a record with "data2", then changes to "data3"
-	c.Set([]byte("record2"), []byte("data2"))
-	c.SetField([]byte("record2"), 0, []byte("data3"))
+	collection.Set([]byte("record2"), []byte("data2"))
+	collection.SetField([]byte("record2"), 0, []byte("data3"))
 
 	// You cannot overwrite records
-	err = c.Add([]byte("record2"), []byte("data4"))
+	err = collection.Add([]byte("record2"), []byte("data4"))
 	if err != nil {
 		fmt.Println("Expected error (collision):", err)
 	}
@@ -38,7 +38,7 @@ func main() {
 
 	// Fetching existing data
 
-	record, record_fetching_error := c.Get([]byte("record")).Record()
+	record, record_fetching_error := collection.Get([]byte("record")).Record()
 	recordFound := record.Match()
 	data, recordNotFoundError := record.Values()
 
@@ -53,7 +53,7 @@ func main() {
 	fmt.Println("Now fetching some non-existing data :")
 	fmt.Println("")
 
-	record, record_fetching_error = c.Get([]byte("nonexisting-record")).Record()
+	record, record_fetching_error = collection.Get([]byte("nonexisting-record")).Record()
 	recordFound = record.Match()
 	data, recordNotFoundError = record.Values()
 
@@ -71,10 +71,10 @@ func main() {
 	fmt.Println("-------------")
 
 	// Verifier needs to have the same type (collections.Data{}) as the collection
-	v := collections.EmptyVerifier(collections.Data{})
+	verifier := collections.EmptyVerifier(collections.Data{})
 
 	// a verifier (who does not already have "record") does not accept updates that aren't part of a Proof
-	err = v.Add([]byte("record"), []byte("somedata"))
+	err = verifier.Add([]byte("record"), []byte("somedata"))
 	if err != nil {
 		fmt.Println("Expected error (unknown subtree):", err)
 	}
@@ -82,27 +82,27 @@ func main() {
 	fmt.Println("-------------")
 	fmt.Println("Now fetching some data in the verifier (who does not have any data):")
 
-	record, record_fetching_error = v.Get([]byte("nonexisting-record")).Record()
+	record, record_fetching_error = verifier.Get([]byte("nonexisting-record")).Record()
 	fmt.Println("Record fetching error (doesn't indicate the record exists or not): ", record_fetching_error)
 
-	// you see the difference with c/v, now record_fetching_error is set since v has no idea whether "nonexisting-record"
+	// you see the difference with collection/verifier, now record_fetching_error is set since verifier has no idea whether "nonexisting-record"
 	// exists or not.
 
 	// let's transfer some data to the verifier
 
-	proof, err := c.Get([]byte("record")).Proof() // "record" doesn't have to exist, you can prove absence
+	proof, err := collection.Get([]byte("record")).Proof() // "record" doesn't have to exist, you can prove absence
 
 	// The proof can be send over the network:
-	// buffer := c.Serialize(proof) // A []byte that contains a representation of proof.
-	// proofagain, deserialize_err := c.Deserialize(buffer)
+	// buffer := collection.Serialize(proof) // A []byte that contains a representation of proof.
+	// proofagain, deserialize_err := collection.Deserialize(buffer)
 
-	v.Begin()
+	verifier.Begin()
 
-	if v.Verify(proof) {
+	if verifier.Verify(proof) {
 		fmt.Println("Verifier accepted the proof about \"record\".")
 	}
 
-	proof, err = v.Get([]byte("record")).Proof()
+	proof, err = verifier.Get([]byte("record")).Proof()
 	fmt.Println(proof)
 	fmt.Println()
 	fmt.Println(err) // err is non-nil !!
@@ -110,10 +110,10 @@ func main() {
 	fmt.Println("-------------")
 	fmt.Println("Now fetching some data in the verifier (who has been updated with a proof):")
 
-	err = v.Add([]byte("record"), []byte("data"))
+	err = verifier.Add([]byte("record"), []byte("data"))
 	if err != nil {
 		fmt.Println("Expected error (unknown subtree):", err) // err is non-nil !!
 	}
 
-	v.End()
+	verifier.End()
 }
